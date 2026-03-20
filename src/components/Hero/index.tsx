2b07945b37fe39d1076dc30c54322bb3c2f87cd5
Seweryn Woliński <42438445+saviusz@users.noreply.github.com>
2026-03-20 21:19:37 +0100

    feat: add image to hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,14 +1,21 @@
 import style from "./style.module.css";
 
 function Hero() {
-  return <section className={style.container}>
-      <h1>Latarnik studencki</h1>
-      <p>Witamy w latarniku studenckim</p>
-      <p>
-        Niczym na wyborach. Wypełnij krótki quiz, żeby poznać przedmioty i znaleźć swoją przyszłą
-        speckę
-      </p>
+  return (
+    <section className={style.container}>
+      <div className={style.texts}>
+        <h1>Latarnik studencki</h1>
+        <p>Witamy w latarniku studenckim</p>
+        <p>
+          Niczym na wyborach. Wypełnij krótki quiz, żeby poznać przedmioty i
+          znaleźć swoją przyszłą speckę
+        </p>
+      </div>
+      <div className={style.image}>
+        <img src="specki.png" alt="Tabela specjalizacji" />
+      </div>
     </section>
+  );
 }
 
-export default Hero
+export default Hero;
